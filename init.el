b6a4cb1cdef2e65f84ab08eff08799815c793103
Ken Stevens <kens7601@me.com>
2023-05-23 08:07:17 +0800

    Made some changes, forgot what they were

--- init.el
+++ init.el
@@ -149,6 +149,8 @@
   :config
   (evil-collection-init))
 
+(use-package evil-smartparens)
+
 (nvmap :keymaps 'override :prefix "SPC"
   "SPC"   '(counsel-M-x :which-key "M-x")
   "c c"   '(compile :which-key "Compile")
@@ -755,3 +757,16 @@
 (use-package smex
   :init
   (smex-initialize))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yaml-mode which-key visual-fill-column use-package typescript-mode toml-mode sudo-edit smex rust-mode rainbow-delimiters pyvenv python-mode org-present-remote org-journal org-bullets no-littering lsp-ui lsp-ivy ivy-rich ivy-prescient helpful gotest go-snippets go-mode general forge evil-smartparens evil-nerd-commenter evil-collection eterm-256color eshell-git-prompt epresent doom-themes doom-modeline dired-single dired-open dired-hide-dotfiles dap-mode counsel-projectile company-box command-log-mode auto-package-update all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
